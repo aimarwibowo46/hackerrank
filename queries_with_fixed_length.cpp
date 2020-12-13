af6b7a85cc6558e5554bec7ed242cf79b8e954a8
Aimar Wibowo <aimarwibowo46@gmail.com>
2020-12-13 18:23:41 +0700

    Update HackerRank solutions

--- queries_with_fixed_length.cpp
+++ queries_with_fixed_length.cpp
@@ -13,11 +13,12 @@ int main() {
 
     while(q--) {
         //deqMax menampung nilai max masing2 subarray
-        //deqMin menampung nilai min dari deqMax
-        deque<int> deqMax, deqMin; 
+        //nilai max subarray disimpan di depan
+        deque<int> deqMax;
+        int min = 1000001;
         cin >> d;
 
-        for(int i = 0; i < d; i++) { //nilai max subarray disimpan di depan
+        for(int i = 0; i < d; i++) { 
             while(!deqMax.empty() && arr[i] > deqMax.back()) {
                 deqMax.pop_back();
             }
@@ -25,11 +26,8 @@ int main() {
         }
 
         for(int i = d; i < n; i++) {
-            if(deqMin.empty()) { //nilai minimal dari deqMax disimpan di belakang
-                deqMin.push_back(deqMax.front());
-            }else if(!deqMin.empty() && deqMax.front() < deqMin.back()) {
-                deqMin.pop_back();
-                deqMin.push_back(deqMax.front());
+            if(deqMax.front() < min) {
+                min = deqMax.front();
             }
 
             if(!deqMax.empty() && deqMax.front() == arr[i-d]) {
@@ -42,10 +40,10 @@ int main() {
             deqMax.push_back(arr[i]);
         }
 
-        if(deqMin.empty() || (deqMax.front() < deqMin.back())) { //deqMin empty untuk kasus n = d
+        if(deqMax.front() < min) { 
             cout << deqMax.front() << endl;
         } else {
-            cout << deqMin.back() << endl;
+            cout << min << endl;
         }
        
     }
